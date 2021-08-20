6fb255ff783339f4085ba27a65530d0b008284ff
nhmunna <munna1720.nhm@gmail.com>
2021-08-21 02:26:36 +0600

    finished main part

---
+++ js/app.js
@@ -0,0 +1,70 @@
+function getTotal() {
+    const bestPriceInput = document.getElementById('best-price');
+    const bestPriceValue = parseInt(bestPriceInput.innerText);
+    const extraMemoryCostInput = document.getElementById('extra-memory-cost');
+    const extraMemoryCostValue = parseInt(extraMemoryCostInput.innerText);
+    const extraStorageCostInput = document.getElementById('extra-storage-cost');
+    const extraStorageCostValue = parseInt(extraStorageCostInput.innerText);
+    const deliveryChargeInput = document.getElementById('delivery-charge');
+    const deliveryChargeValue = parseInt(deliveryChargeInput.innerText);
+    const total = bestPriceValue + extraMemoryCostValue + extraStorageCostValue + deliveryChargeValue;
+    return total;
+}
+
+function totalPrice(totalPriceId) {
+    const totalInput = document.getElementById(totalPriceId);
+    totalInput.innerText = getTotal();
+    return totalInput.innerText;
+}
+
+document.getElementById('memory-btn-8gb').addEventListener('click', function () {
+    document.getElementById('extra-memory-cost').innerText = 0;
+    // const totalInput = document.getElementById('total-price');
+    // totalInput.innerText = getTotal();
+    totalPrice('total-price');
+    totalPrice('discount-total');
+})
+document.getElementById('memory-btn-16gb').addEventListener('click', function () {
+    document.getElementById('extra-memory-cost').innerText = 180;
+    // const totalInput = document.getElementById('total-price');
+    // totalInput.innerText = getTotal();
+    totalPrice('total-price');
+    totalPrice('discount-total');
+})
+document.getElementById('storage-btn-256gb').addEventListener('click', function () {
+    document.getElementById('extra-storage-cost').innerText = 0;
+    // const totalInput = document.getElementById('total-price');
+    // totalInput.innerText = getTotal();
+    totalPrice('total-price');
+    totalPrice('discount-total');
+})
+document.getElementById('storage-btn-512gb').addEventListener('click', function () {
+    document.getElementById('extra-storage-cost').innerText = 100;
+    // const totalInput = document.getElementById('total-price');
+    // totalInput.innerText = getTotal();
+    totalPrice('total-price');
+    totalPrice('discount-total');
+})
+document.getElementById('storage-btn-1tb').addEventListener('click', function () {
+    document.getElementById('extra-storage-cost').innerText = 180;
+    // const totalInput = document.getElementById('total-price');
+    // totalInput.innerText = getTotal();
+    totalPrice('total-price');
+    totalPrice('discount-total');
+})
+document.getElementById('delivery-cost-free').addEventListener('click', function () {
+    document.getElementById('delivery-charge').innerText = 0;
+    // const totalInput = document.getElementById('total-price');
+    // totalInput.innerText = getTotal();
+    totalPrice('total-price');
+    totalPrice('discount-total');
+})
+document.getElementById('delivery-cost-price').addEventListener('click', function () {
+    document.getElementById('delivery-charge').innerText = 20;
+    // const totalInput = document.getElementById('total-price');
+    // totalInput.innerText = getTotal();
+    totalPrice('total-price');
+    totalPrice('discount-total');
+})
+
+
